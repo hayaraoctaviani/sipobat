3772f75a71b1371e4ef6633b27f22c83fdfb5853
hayaraoctaviani <hayaraoctaviani@gmail.com>
2022-03-21 05:46:25 +0700

    first

---
+++ app/Controllers/Users/Admin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers\Users;
+use App\Controllers\BaseController;
+
+class Admin extends BaseController
+{
+    public function index()
+    {
+        echo "ini halaman admin";
+    }
+}
---
+++ app/Controllers/Users/User.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\Users;
+use App\Controllers\BaseController;
+
+
+class User extends BaseController
+{
+    public function index()
+    {
+        echo "ini halaman user";
+    }
+}
